5e25bd4331f178bbe0053cc456a25c793eefbb62
Neff10 <st.tunkara@gmail.com>
2019-05-16 22:58:26 +0100

    Add 6 TV show titles #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import daredevil from './daredevil-cover.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <img src={daredevil} alt="Logo" />
+    <div className='App'>
+          <img src={daredevil} alt="Logo" />
+      <div className='container'>
+        <div className='item'>Daredevil</div>
+        <div className='item'>The Mentalist</div>
+        <div className='item'>My name is earl</div>
+      </div>
+      <div className='container'>
+        <div className='item'>Shameless</div>
+        <div className='item'>Vikings</div>
+       <div className='item'>Supernatural</div>
+      </div>
     </div>
   );
 }

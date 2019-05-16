837682825e373b4737c66530e2e208041db0b94c
Neff10 <st.tunkara@gmail.com>
2019-05-16 23:11:52 +0100

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import daredevil from './daredevil-cover.jpg'
+import mentalist from './mentalist-cover.jpg'
+import earl from './earl-cover.jpg'
+import shameless from './shameless-cover.jpg'
+import vikings from './vikings-cover.jpg'
+import supernatural from './supernatural-cover.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-          <img src={daredevil} alt="Logo" />
       <div className='container'>
-        <div className='item'>Daredevil</div>
-        <div className='item'>The Mentalist</div>
-        <div className='item'>My name is earl</div>
+        <div className='item'>
+         <img src={daredevil} alt="Daredevil"/>
+        </div>
+        <div className='item'>
+         <img src={mentalist} alt="The Mentalist"/>
+        </div>
+        <div className='item'>
+         <img src={earl} alt="My name is earl"/>
+        </div>
       </div>
+
       <div className='container'>
-        <div className='item'>Shameless</div>
-        <div className='item'>Vikings</div>
-       <div className='item'>Supernatural</div>
+        <div className='item'>
+         <img src={shameless} alt="Shameless"/>
+        </div>
+        <div className='item'>
+         <img src={vikings} alt="Vikings"/>
+        </div>
+        <div className='item'>
+         <img src={supernatural} alt="Supernatural"/>
+        </div>
       </div>
     </div>
   );
